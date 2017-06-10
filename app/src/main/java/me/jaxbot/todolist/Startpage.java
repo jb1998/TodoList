77d1665a8265f4e442038caa2f433a1a49b37ca2
jb1998 <jatinbindra171998@gmail.com>
2017-06-11 02:19:53 +0530

    TodoListAndroidApp

--- app/src/main/java/me/jaxbot/todolist/Startpage.java
+++ app/src/main/java/me/jaxbot/todolist/Startpage.java
@@ -4,31 +4,53 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.view.animation.TranslateAnimation;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
 
-public class Startpage extends AppCompatActivity implements View.OnClickListener {
-Button b;
+public class Startpage extends AppCompatActivity  {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startpage);
-     b=(Button)findViewById(R.id.button);
+        ImageView imageView = (ImageView)findViewById(R.id.image);
+        Animation animation= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.animation);
+        imageView.setAnimation(animation);
 
-        b.setOnClickListener(this);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finish();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
 
 
     }
 
-    @Override
-    public void onClick(View view) {
-        Intent i = new Intent();
-        i.setClass(Startpage.this,MainActivity.class);
-        startActivity(i);
-    }
+
 }
